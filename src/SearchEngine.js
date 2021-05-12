7da8a5004f87739b9a466168e8c35bafcfa09bca
AntonellaDiazPico <Anto@192-168-1-103.tpgi.com.au>
2021-05-12 11:55:56 +1000

    Added API integration

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,26 +1,29 @@
+import axios from "axios";
 import React, { useState } from "react";
-import "./SearchEngine.css"
+import "./SearchEngine.css";
 
 export default function SearchEngine() {
-    const [query, setQuery] = useState(null);
+  const [query, setQuery] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${query}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        alert(`searching ${query}`);
-    }
-
-    function handleQuery(event) {
-        event.preventDefault();
-        setQuery(event.target.value);
-
-    }
-    return (
-        <div className="SearchEngine">
-            <form  onSubmit={handleSubmit}>
-                <input type="search" onChange={handleQuery} className="searchForm"/>
-            </form>
-        </div>
-    );
+  function handleQuery(event) {
+    event.preventDefault();
+    setQuery(event.target.value);
+  }
+  return (
+    <div className="SearchEngine">
+      <form onSubmit={handleSubmit}>
+        <input type="search" onChange={handleQuery} className="searchForm" />
+      </form>
+    </div>
+  );
 }
